0da3ff1e894d448cc9b09772c6d7b7b4c45a5093
Jonas Arnfred <jonas@ifany.org>
2012-04-26 12:04:56 +0000

    Adding a few files to the repositor

--- mmse_mrf_demo/solvesles.m
+++ mmse_mrf_demo/solvesles.m
@@ -8,12 +8,14 @@ function [result elapsed] = solvesles()
 
   % Add path
   addpath('mat');
+  addpath('inf');
 
   % Get mrf
   mrf = learned_models.cvpr_3x3_foe;
 
   % Express B in terms of the filters of the mrf
-  B = ones(0,prod(dims));
+  % B = ones(0,prod(dims));
+  B = mat([0 prod(dims)], 'valid');
 
   % For each filter, create a fake convolution matrix
   for i=1:mrf.nfilters
@@ -28,17 +30,18 @@ function [result elapsed] = solvesles()
   [~, number_of_zs] = size(zes);
 
   % Set up timer
-  elapsed = 0;
-  tstart = cputime;
+  elapsed = zeros(3,1);
+  tstart = zeros(3,1);
 
   % Set up return matrix as the last 4 matrices:
-  result = zeros(dims(1), dims(2), 4);
+  result = zeros(dims(1), dims(2), 3);
 
   % Number of rows in diagonal (dims - 2 because the 'valid' fft transform gives back a smaller matrix) 
   N = prod(dims - 2) * mrf.nfilters; % size of filter (80*80) times number of filters (8)
 
   % Iterate over all Z's, solving the systems one by one
-  for i = 1:number_of_zs
+  % for i = 1:number_of_zs
+  for i = 1:5
 	  
 	  % Get diagonal matrix
 	  origDiagonal	= full(cell2mat(zes(i)));
@@ -50,27 +53,45 @@ function [result elapsed] = solvesles()
 	  % Calculate right hand side
 	  rhs			= B' * sqrtDiagonal * randn(N, 1); % n0 in notes
 
-	  % Solve system of: (B' * diagonal * B + P' * R * P) * u = rhs
-	  % Input are: B, diagonal, P, R, u, max MVM's, conditioner (I think), treshold, use_preconditioner
-	  % These aren't exactly the same names as used in the documentation
-	  P				= matDiag(zeros(1,prod(dims)));
-	  R				= zeros(prod(dims),1);
-	  maxMVMs		= 400;
-	  conditioner	= zeros(prod(dims), 1);
-	  treshold		= 1e-6;
-	  use_precond	= 0;
+	  % Prepare preconditioners
+	  A				= B'*diagonal*B;
+	  diag_A		= diag(A);
+	  d 			= mean(diag(diagonal)) * diagFAtAFt(B,'cheap');
+	  F 			= matFFTNmask(true(size(d))); % DFT matrix
+	  M1			= @(r) r;
+	  M2			= @(r) r ./ diag_A;
+	  M3			= @(r) mvmMinv(F, d(:), r);
+
 
 	  % Plug in arguments and calculate away, time the operation
-	  tstart		= cputime;
-	  u				= linsolve_lcg(B, diagonal, P, R, rhs, maxMVMs, conditioner, treshold, use_precond);
+	  tstart(1)		= cputime;
+	  [u1, iter1]	= conjugate_gradients(A, rhs, M1);
+	  elapsed(1)	= elapsed(1) + (cputime - tstart(1));
 
-	  % Calculate elapsed time
-	  elapsed		= elapsed + (tstart - cputime);
+	  % Now do the same, but with diag(A) preconditioning
+	  tstart(2)		= cputime;
+	  [u2, iter2]	= conjugate_gradients(A, rhs, M2);
+	  elapsed(2)	= elapsed(2) + (cputime - tstart(2));
 
-	  % Save image vector in result matrix
-	  n				= mod(i, 4) + 1;
-	  result(:,:,n)	= reshape(u, dims);
+	  % Now with diagonal in fuirier space as preconditioning
+	  tstart(3)		= cputime;
+	  [u3, iter3]	= conjugate_gradients(A, rhs, M3);
+	  elapsed(3)	= elapsed(3) + (cputime - tstart(3));
 
   end
+
+  result(:,:,1)	= reshape(u1, dims);
+  result(:,:,2)	= reshape(u2, dims);
+  result(:,:,3)	= reshape(u3, dims);
+
 	  
 end
+
+  % M\r in the DFT domain (From hannes nickish code)
+  function z = mvmMinv(F,d, r)
+	  if numel(r)>numel(d)
+		  z = cx2re([F']*((F*re2cx(r))./d));
+	  else
+		  z = [F']*((F*r)./d);
+	  end
+  end
